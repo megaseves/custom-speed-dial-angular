27fd8ab0afc9e3c85d04d008bf035b2efce073b1
megaseves <megaseves@gmail.com>
2023-07-01 14:06:39 +0200

    change some config for setup

--- chrome/js/background.js
+++
@@ -1,8 +0,0 @@
-chrome.tabs.onCreated.addListener(function(tab) {
-  chrome.notifications.create({
-    type: 'basic',
-    iconUrl: '../images/spoon_icon16.png',
-    title: 'Új lap nyitva',
-    message: tab.url
-  });
-});
